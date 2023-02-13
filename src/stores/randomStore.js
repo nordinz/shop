610d2d69eeb326fde7a2900e9fd8271cc37996ad
nordinz <112375926+nordinz@users.noreply.github.com>
2023-02-13 17:39:46 +0100

    adding dist

--- src/stores/randomStore.js
+++
@@ -1,9 +0,0 @@
-import { defineStore } from "pinia";
-
-export const useRandomStore = defineStore("randomStore", {
-  state: () => ({
-    productpage: "Welcome to the product section!",
-    producttext:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est culpa officiis vel natus doloremque, aliquid dignissimos expedita nihil incidunt ad dolor voluptatem maiores possimus cupiditate quaerat. Nemo velit soluta incidunt asperiores similique ab est, amet esse architecto ad eum officia.",
-  }),
-});
